34387c139f04c3fee64ba606efab611198120e5f
afrozamitu <afrozamitulive@gmail.com>
2023-06-09 19:22:50 +0600

    404 page added

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import image from '../../assets/404.gif'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div className=' bg-[#F0F0F0]'>
-            <div className='h-[100vh] md:w-7/12 md:mx-auto' style={{ backgroundImage: `url("${image}")` , backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-            <h1>xisjheruvn</h1>
+            <div className='h-[100vh] md:w-7/12 md:mx-auto relative' style={{ backgroundImage: `url("${image}")` , backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+            <Link to='/' className='px-5 py-2 bg-green-700 hover:opacity-80 rounded text-white font-bold text-lg absolute bottom-16 left-[350px]' style={{transition: '.5s'}}>Go Back</Link>
         </div>
         </div>
         
